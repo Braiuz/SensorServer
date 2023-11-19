aec28f9353b900254023667f38ce7a54c0afdb09
Braiuz <matteobraiato96@gmail.com>
2023-11-19 17:03:40 +0100

    ADD timer with interrupt

--- main.py
+++ main.py
@@ -4,22 +4,42 @@ from dht import DHT11
 from led import Led
 import micropython          # For emergency exception buffer
 
-## Init ##
-led = Led()
-micropython.alloc_emergency_exception_buf(100)      # For emergency exception buffer
-timerTask1ms = Timer()                              # TODO fai libreria timersw
-## -- ##
+## GLOBAL VAR ##
+temperature = 0.0
+humidity = 0.0
 
-while True:
+
+## Callback ##
+def timerCallback(timer):
+    global temperature, humidity
     try:
-        sleep(5)
-        pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
-        sensor = DHT11(pin)
-        t  = (sensor.temperature)
-        h = (sensor.humidity)
-        print("Temperature: {}".format(sensor.temperature))
-        print("Humidity: {}".format(sensor.humidity))
+        temperature = (sensor.temperature)
+        humidity = (sensor.humidity)
     except Exception as e:
         print("Exception: \"" + str(e)+"\"")
+
+
+## Init ##
+print("Pi initialization")
+led = Led()
+
+pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+sensor = DHT11(pin)
+
+micropython.alloc_emergency_exception_buf(100)      # For emergency exception buffer
+timerTask5s = Timer()                               # TODO fai libreria timersw
+timerTask5s.init(period=5000, callback=timerCallback)
+print("Pi initialization done")
+## -- ##
+
+
+## Main loop ##
+print("Main loop start")
+while True:
+    sleep(5)
+    print("Temperature: {}".format(temperature))
+    print("Humidity: {}".format(humidity))
     
     led.toogle()
+
+## -- ##
