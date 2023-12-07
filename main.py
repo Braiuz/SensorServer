8bf521ad78cd77231141a12a68fd61ea3ae7e34e
MatteoBraiato <ex071991@piaggio.com>
2023-12-07 14:58:29 +0100

    usocket init and pack bytes

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from dht import DHT11
 from led import Led
 import micropython          # For emergency exception buffer
 import network
-import socket
+import usocket as socket
+import utime
+from struct import pack
 
 ## GLOBAL VAR ##
 temperature = 0.0
 humidity = 0.0
+time = 0
 
 ssid = "Barbagianni"
 password = "ciccinivolantifulminati"
@@ -18,10 +21,11 @@ serverPort = "20000"
 
 ## Callback ##
 def timerCallback(timer):
-    global temperature, humidity
+    global temperature, humidity, time
     try:
         temperature = (sensor.temperature)
         humidity = (sensor.humidity)
+        time = utime.time()
     except Exception as e:
         print("Exception: \"" + str(e)+"\"")
 
@@ -32,7 +36,18 @@ def connect():
     while wlan.isconnected() == False:
         print("Waiting for connection...")
         sleep(1)
-    print(wlan.ifconfig())
+    ip = wlan.ifconfig()[0]
+    print(f"Connected on {ip}", ip)
+    return ip
+
+
+def SocketSend(sock: socket.socket, msg, msgLen):
+    totalsent = 0
+    while totalsent < msgLen:
+        sent = sock.send(msg[totalsent:])
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
+        totalsent = totalsent + sent
 
 
 ## Init ##
@@ -46,16 +61,16 @@ micropython.alloc_emergency_exception_buf(100)      # For emergency exception bu
 timerTask5s = Timer()                               # TODO fai libreria timersw
 timerTask5s.init(period=5000, callback=timerCallback)
 
+# WLAN connection
 try:
-    connect()
+    ip = connect()
 except KeyboardInterrupt:
     reset()
 
-
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.connect((serverIp, serverPort))
-
-
+# Socket init
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverIp, serverPort))
+print("Connected to: " + serverIp + ":" + serverPort)
 
 print("Pi initialization done")
 ## -- ##
@@ -68,6 +83,10 @@ while True:
 
     print("Temperature: {}".format(temperature))
     print("Humidity: {}".format(humidity))
+
+    msgBytes = pack('<iff', time, temperature, humidity)
+
+    SocketSend(clientSocket, msg=msgBytes, msgLen=len(msgBytes))
     
     led.toogle()
 
